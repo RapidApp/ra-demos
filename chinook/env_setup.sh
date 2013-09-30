326d8d9d485e0b446e6bb7857a7768fd6a145d69
Henry Van Styn <vanstyn@intellitree.com>
2013-09-30 16:17:01 -0400

    chinook: env_setup.sh enhanced to jump into middle of demo

--- chinook/env_setup.sh
+++ chinook/env_setup.sh
@@ -11,23 +11,46 @@
 # demo. It sets a custom/simple prompt and sets up a 
 # custom/dedicated history file in basic (no timestamp)
 # mode.
+#
+# UPDATE: also sets up aliases and can be ran any time
+# to be able to jump back into the demo in the middle
+# with correct history
 
 ## tmp for dev:
 export PERLLIB=/root/github/RapidApp/lib
 export RAPIDAPP_SHARE_DIR=/root/github/RapidApp/share
 
-### Clear any previous work
-rm -rf RA-ChinookDemo/
+# quick/dirty find vimrc file in either parent or grandparent dir
+vimrc_file="$PWD/../vimrc"
+if [ ! -f $vimrc_file ]; then
+  vimrc_file="$PWD/../../vimrc"
+fi
 
 # for .vimrc
-cd ..
-export VIMINIT="source $PWD/vimrc"
-cd -
+export VIMINIT="source $vimrc_file"
+
+## ----
+## Setup Commit aliases
+##
+## This is duplicated in part 1, but is also here
+## to allow jumping in at a later point:
+alias Commit='\
+    history -a cmd_history.sh && \
+    RestoreHistNewlines cmd_history.sh && \
+    git add --all && \
+    git commit -m'
+
+alias RestoreHistNewlines='\
+  sed -i -e \
+   '"'"'/\\$/,/[^\\]$/{p;d;};/^[a-z]/s/ \( \) *\([^#]\)/ \\\n \1 \2/g'"'"''
+##
+## ----
 
 ### setup prompt and history
 export PS1='\[\033[01;34m\] \W >\[\033[00m\] '
 unset HISTTIMEFORMAT
 shopt -s cmdhist lithist
-rm -f ~/cmd_history.sh
-export HISTFILE=~/cmd_history.sh
+rm -f ~/ra_demo_tmp_history.txt
+export HISTFILE=~/ra_demo_tmp_history.txt
 history -c && clear
+## --
